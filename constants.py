d1157c49014eefda9bf01a8d4abd0590c53a4166
Jerry Xu <Jerry Xu>
2017-10-24 21:52:20 -0400

    created reflections

---
+++ constants.py
@@ -0,0 +1,33 @@
+
+reflections = {
+	"i": "you",
+	"i'd": "you would",
+	"i've": "you have",
+	"i'll": "you will",
+	"you": "me", # check this... me or I??
+	"you've": "I have",
+	"you'll": "I will",
+	"you'd": "I would",
+	"your": "my",
+	"yours": "mine",
+	"me": "you",
+	"my": "your",
+	"mine": "yours", # check this 
+	"am": "are",
+	"are": "am",
+	"was": "were",
+	"were": "was" # check this
+}
+
+patterns = [
+	[]
+
+
+
+
+
+]
+
+
+
+
