1693a94cd57b10fb68c7f4dd004de6f8b81a9996
jerryxu178 <lx46@cornell.edu>
2017-10-26 21:45:37 -0400

    added documentation

--- constants.py
+++ constants.py
@@ -1,4 +1,25 @@
 
+"""
+patterns_list contains the patterns that the chatbot needs for pattern matching.
+
+patterns_list is implemented as a list of lists, where each element contains a 
+pattern that may match the user's statement and also a list of responses that
+the chatbot choose from to echo back the user's original statement.
+"""
+patterns_list = [
+	[r'i need (.*)',
+	["Why do you need {0}?",
+	"Would it really help you to get {0}?",
+	"Are you sure you need {0}?"]]
+]
+
+
+"""
+reflections_dict is a dictionary that the chatbot uses to convert words so that
+statements that are echoed back to the user make sense.
+
+MORE EXPLANATION HERE. MAYBE AN EXAMPLE
+"""
 reflections_dict = {
 	"i": "you",
 	"i'd": "you would",
@@ -18,14 +39,6 @@ reflections_dict = {
 	"was": "were",
 	"were": "was" # check this
 }
-
-patterns_list = [
-	[r'i need (.*)',
-	["Why do you need {0}?",
-	"Would it really help you to get {0}?",
-	"Are you sure you need {0}?"]]
-]
-
 
 
 
