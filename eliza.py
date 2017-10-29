707152e8035576a56ecfe6f1a9425086651311ea
jerryxu178 <lx46@cornell.edu>
2017-10-28 21:19:35 -0400

    implemented generate_response

--- eliza.py
+++ eliza.py
@@ -2,12 +2,24 @@ import re
 import random
 from constants import reflections_dict, patterns_list
 
+
+def reflect(fragment):
+	"""
+	documentation here
+	"""
+	return fragment
+
 def generate_response(statement):
-"""
-documentation here
-"""
+	"""
+	documentation here
+	"""
 	for pattern, responses in patterns_list:
-		re.match(pattern, statement.rstrip(".!?"))
+		match = re.match(pattern, statement.rstrip(".!?")) # match is None or a Match object
+		if match:
+			response = random.choice(responses) # response with {} as placeholder for user text
+			fragment = reflect(match.group(1)) # piece of user text to be echoed back in response
+			# convert "I" to "you", and other similar changes
+			return response.format(fragment)
 
 def main():
 	print "Hello. How are you feeling today?"
