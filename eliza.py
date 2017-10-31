d72a6ebe9a7b3208de94a21863b51a8a89203440
jerryxu178 <lx46@cornell.edu>
2017-10-31 18:02:41 -0400

    updated documentation

--- eliza.py
+++ eliza.py
@@ -5,7 +5,11 @@ from constants import reflections_dict, patterns_list
 
 def reflect(fragment):
 	"""
-	documentation here
+	reflect takes a string and replaces any words that have a corresponding
+	reflection. For example, "my house" is returned as "your house". 
+
+	reflect() looks up reflections in reflections_dict, and is used in 
+	generate_response()
 	"""
 	word_list = fragment.split(" ")
 	for index in range(len(word_list)):
@@ -16,13 +20,20 @@ def reflect(fragment):
 
 def generate_response(statement):
 	"""
-	documentation here
+	generate_response takes the input string and returns a response, based on
+	the patterns that the input matches. The returned response takes the form
+	of a string.
 	"""
 	for pattern, responses in patterns_list:
-		match = re.match(pattern, statement.rstrip(".!?")) # match is None or a Match object
+		match = re.match(pattern, statement.rstrip(".!?")) 
 		if match:
-			response = random.choice(responses) # response with {} as placeholder for user text
-			fragment = reflect(match.group(1)) # piece of user text to be echoed back in response
+			# response is the bot's reply, with {} as placeholder for user text
+			response = random.choice(responses) 
+			try:
+				# fragment contains the piece of user text to be echoed back
+				fragment = reflect(match.group(1)) 
+			except IndexError:
+				fragment = ""
 			# convert "I" to "you", and other similar changes
 			return response.format(fragment)
 
@@ -31,6 +42,8 @@ def main():
 	while True:
 		statement = raw_input(">")
 		print generate_response(statement.lower())
+		if statement.lower() == "quit":
+			break
 
 if __name__ == "__main__":
 	main()
